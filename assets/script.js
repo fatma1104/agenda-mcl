dcfbe9aad99d9cab490983f34e0d230ceb810cb7
fatma1104 <berchichefatmazohra@gmail.com>
2025-05-13 12:42:04 +0100

    modification script: dinamisation des pages

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//  confirmation avant suppression
+// confirmation avant suppression
 document.addEventListener("DOMContentLoaded", () => {
     const deleteButtons = document.querySelectorAll(".btn-delete");
 
@@ -11,3 +11,42 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+ // Gestion des modals
+        function openModal(id) {
+            document.getElementById(id).style.display = 'flex';
+        }
+        
+        function closeModal(id) {
+            document.getElementById(id).style.display = 'none';
+        }
+        
+        function openShareModal(calendarId) {
+            document.getElementById('share-calendar-id').value = calendarId;
+            openModal('share-calendar-modal');
+        }
+        
+        // Fermer en cliquant à l'extérieur
+        window.onclick = function(event) {
+            if (event.target.className === 'modal') {
+                document.querySelectorAll('.modal').forEach(modal => {
+                    modal.style.display = 'none';
+                });
+            }
+        }
+         // Gestion des onglets
+        function showTab(tabId) {
+            // Masquer tous les contenus
+            document.querySelectorAll('.tab-content').forEach(content => {
+                content.classList.remove('active');
+            });
+            
+            // Désactiver tous les onglets
+            document.querySelectorAll('.tab').forEach(tab => {
+                tab.classList.remove('active');
+            });
+            
+               // Activer l'onglet sélectionné
+            document.getElementById(tabId).classList.add('active');
+            event.currentTarget.classList.add('active');
+        }
